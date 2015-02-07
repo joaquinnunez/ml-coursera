e09a998314cbab789bd62023d66a62ec6d5ad551
Joaquin Nunez <contacto@joaquinnunez.cl>
2015-02-07 18:46:44 -0300

    Programming Exercise 1: Linear Regression.
    featureNormalization, computeCostMulti and gradientDescentMulti finished

--- mlclass-ex1-008/mlclass-ex1/gradientDescentMulti.m
+++ mlclass-ex1-008/mlclass-ex1/gradientDescentMulti.m
@@ -1,5 +1,5 @@
 function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters)
-%GRADIENTDESCENTMULTI Performs gradient descent to learn theta
+%   GRADIENTDESCENTMULTI Performs gradient descent to learn theta
 %   theta = GRADIENTDESCENTMULTI(x, y, theta, alpha, num_iters) updates theta by
 %   taking num_iters gradient steps with learning rate alpha
 
@@ -11,25 +11,17 @@ for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
+    %               theta.
     %
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
 
-
-
-
-
-
-
-
-
-
+    theta = theta - (alpha / m) * (X' * (X * theta - y));
 
     % ============================================================
 
-    % Save the cost J in every iteration    
+    % Save the cost J in every iteration
     J_history(iter) = computeCostMulti(X, y, theta);
 
 end
